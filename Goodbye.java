21afb469335d8c9b149ee88bed8d979d20123f3d
hayton au <haytonau@gmail.com>
2025-08-21 15:55:04 +0800

    OK

---
+++ Goodbye.java
@@ -0,0 +1,19 @@
+public class Goodbye {
+  public static void main(String[] args) {
+    System.out.println("goodbye");
+    System.out.println("FUCK");
+    System.out.println("\"");
+    int x = 3;
+    int y = 7;
+    System.out.println("x");
+    System.out.println(x);
+    x = 10;
+    System.out.println(x);
+    double U = 5.5;
+    System.out.println(U);
+    int x2 = 3;
+    int x3 = 10;
+    x2 = x3;
+    System.out.println(x3);
+  }
+}
